90f8af7814f122bd9bc5e3b6bf541f8f26a5dae8
Nikolay Nikitin <nikitin.na@phystech.edu>
2019-07-05 02:04:17 +0300

    tree_add

--- tree_func.c
+++ tree_func.c
@@ -6,7 +6,7 @@
 /*   By: crycherd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 01:10:41 by crycherd          #+#    #+#             */
-/*   Updated: 2019/07/05 01:19:53 by crycherd         ###   ########.fr       */
+/*   Updated: 2019/07/05 02:04:01 by crycherd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,28 @@ t_tree	*tree_create(char *name)
 		new->pre = NULL;
 	}
 	return (new);
+}
+
+t_tree *tree_addend(t_tree *start, t_tree *new)
+{
+	if (start)
+	{
+		while (start->next)
+			start = start->next;
+		start->next = new;
+		new->pre = start;
+		new->par = start->parent;
+	}
+	return (new);
+}
+
+t_tree *tree_addend_chil(t_tree *root, t_tree *leaf)
+{
+	leaf = tree_addend(root->chi, leaf);
+	if (!root->chi)
+	{
+		root->chi = leaf;
+		leaf->par = root;
+	}
+	return (leaf);
 }
