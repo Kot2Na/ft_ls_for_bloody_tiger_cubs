3cac4b665e4c7648ecf2d76c02419e984f0f0722
Bret Omanyte <bomanyte@at-c3.21-school.ru>
2019-07-07 17:00:25 +0300

    fixed some things in your code - see it. and fill data almost done

--- fill_data.c
+++ fill_data.c
@@ -6,59 +6,169 @@
 /*   By: bomanyte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:04:29 by bomanyte          #+#    #+#             */
-/*   Updated: 2019/07/07 12:58:04 by bomanyte         ###   ########.fr       */
+/*   Updated: 2019/07/07 16:58:35 by bomanyte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libls.h"
 #include <stdio.h>
 
-void	get_time(static stat buff, t_tree *node)
+/*void	file_set_zero(t_tree *node, int errno)
 {
-	char *buff;
+	node->user = NULL;
+	node->group = NULL;
+	node->hd_link = NULL;
+	node->time = NULL;
+	node->size = NULL;
+	node->rights = NULL;
+	node->error = errno;
+} */
+
+//void	get_id(struct stat *buff, t_tree *node)
+void	get_id(struct stat *buff)
+{
+	char *uid;
+	char *gid;
+	struct passwd *user_id;
+	struct group *group_id;
+	user_id = getpwuid(buff->st_uid);
+	group_id = getgrgid(buff->st_gid);
+	uid = ft_strdup(user_id->pw_name);
+	gid = ft_strdup(group_id->gr_name);
+	//node->user = uid;
+	//node->group = gid;
+}
+
+//void		get_link(struct stat *buff, t_tree *node)
+void		get_link(struct stat *buff)
+{
+	unsigned short hd_link;
+	if (!(hd_link = (unsigned short)malloc(sizeof(buff->st_nlink))))
+		perror("malloc");
+	hd_link = buff->st_nlink;
+	//node->hd_link = hd_link;
+}
+
+//void	get_time(static stat buff, t_tree *node)
+void	get_time(struct  stat *buff)
+{
+	char *buf;
 	char *time;
 	char copy[13];
 	int i;
 	int j;
 
 	i = 0;
-	j = 0;
-	buf = ctime(&buff.st_mtime);
-	while (copy[i] != '\0')
+	j = 4;
+	buf = ctime(&buff->st_mtime);
+	while (i != 12)
 	{
 		copy[i] = buf[j];
 		i++;
 		j++;
 	}
 	time = ft_strdup(copy);
-	node->time = time;
+	printf("%s\n", time);
+	//node->time = time;
 }
 
-void	fill_data(t_tree *node, char *name)
+void	get_size(struct stat *buff)
 {
+	long long size;
+	size = (long long)malloc(sizeof(buff->st_size));
+	size = buff->st_size;
+	//node->size = size;
+}
+
+int		get_soft_ln(struct stat *buff, char *path)
+{
+	char *buf;
+	ssize_t len;
+
+	if (S_ISLNK(buff->st_mode))
+	{
+		len = buff->st_size + 1;
+		if (!(buf = (char *)malloc(len)))
+		{
+			perror("malloc");
+			exit(-1);
+		}
+		if (readlink(path, buf, len) == -1)
+		{
+			perror("readlink");
+			exit(-1);
+		}
+		buf[len] = '\0';
+		//node->soft_ln = buf;
+		return (1);
+	}
+/*	node->soft_ln = NULL; */
+	return (0);
+}
+
+void	get_rwx(char *buf, struct stat *buff)
+{
+	buf[1] = (buff->st_mode & S_IRUSR) ? 'r' : '-';
+	buf[4] = (buff->st_mode & S_IRGRP) ? 'r' : '-';
+	buf[7] = (buff->st_mode & S_IROTH) ? 'r' : '-';
+	buf[2] = (buff->st_mode & S_IWUSR) ? 'w' : '-';
+	buf[5] = (buff->st_mode & S_IWGRP) ? 'w' : '-';
+	buf[8] = (buff->st_mode & S_IWOTH) ? 'w' : '-';
+	buf[3] = (buff->st_mode & S_IXUSR) ? 'x' : '-';
+	buf[6] = (buff->st_mode & S_IXGRP) ? 'x' : '-';
+	buf[9] = (buff->st_mode & S_IXOTH) ? 'x' : '-';
+}
+
+void    get_mode(struct stat *buff, char *path)
+{
+	int is_l;
+	char *buf;
+	
+	is_l = 0;
+	buf = (char *)ft_memalloc(10);
+	while (is_l != 10)
+	{
+		buf[is_l] = '-';
+		is_l++;
+	}
+	is_l = 0;
+	if ((is_l = get_soft_ln(buff, path)) == -1)
+		perror("get_soft_ln");
+	get_rwx(buf, buff);
+	buf[0] = is_l ? 'l' : '-';
+	buf[9] = (buff->st_mode & S_ISVTX) ? 't' : buf[9];
+	printf("%s\n", buf);
+}
+
+//void	fill_data(t_tree *node, char *name)
+void	fill_data(char *name)
+{
+	//char *path = "/Users/bomanyte/ft_ls/test.c";
 	struct stat buff;
 	
-	if (errno)
+	if (errno || lstat(name, &buff) == -1)
 	{
-		node->error = errno;
+		//file_set_zero(node, errno);
 		errno = 0;
 		return ;
 	}
 	else
 	{
-		lstat(name, &buff);
-		/*get_id(buff.st_uid, buff.st_gid, node);
-		get_mode(buff.st_mode, node);
-		get_link(buff.st_nlink, node);
-		get_size(buff.st_size, node); */
-		get_time(buff, node);
+		get_id(&buff);
+		get_link(&buff);
+		get_size(&buff);
+		get_mode(&buff, name);
+		get_time(&buff);
+		//node->error = 0;
 	}
 //total
-//st_uid
-//st_gid
 //st_mode (file or directory, permissions, sticky, suid, links)
-//st_size
-//st_mtime (modification)
-//st_nlink
-free(buff);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	fill_data(argv[1]);
+	return (0);
 }
--- libls.h
+++ libls.h
@@ -6,7 +6,7 @@
 /*   By: crycherd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 00:52:26 by crycherd          #+#    #+#             */
-/*   Updated: 2019/07/07 12:49:08 by bomanyte         ###   ########.fr       */
+/*   Updated: 2019/07/07 14:38:38 by bomanyte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 # include <errno.h>
 # include <string.h>
 # include <time.h>
+# include <pwd.h>
+# include <grp.h>
 
 typedef struct dirent	t_dir;
 typedef struct stat		t_stat;
@@ -37,7 +39,9 @@ typedef struct			s_data
 {
 	int					error;
 	char				*rights;
-	int					link;
+	unsigned short		hd_link;
+	char				*soft_ln;
+	char				*user;
 	char				*group;
 	long long			size;
 	char				*time;
@@ -65,6 +69,8 @@ int						is_legal(char c);
 int						validate_flags(char **argv);
 void					set_flags(char **argv, t_bit *flags);
 void					set_zero(t_bit *flags);
-void					fill_data(char *name, t_tree *node);
-void					get_time(static stat buff, t_tree *node);
+//void					fill_data(char *name, t_tree *node);
+void					fill_data(char *name);
+void	get_time(struct stat *buff);
+//void					get_time(struct stat *buff, t_tree *node);
 #endif
