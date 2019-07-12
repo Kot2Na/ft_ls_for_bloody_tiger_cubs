efa4bb3d582b9df58e48ff3dbd81113bd22a1a2b
Bret Omanyte <bomanyte@il-k2.21-school.ru>
2019-07-12 19:23:53 +0300

    fixed flags, ascii time sort,format output

--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 /*   By: crycherd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 01:11:17 by crycherd          #+#    #+#             */
-/*   Updated: 2019/07/11 21:56:32 by crycherd         ###   ########.fr       */
+/*   Updated: 2019/07/11 22:51:26 by bomanyte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libls.h"
-
+#include <stdio.h>
 t_bit *create_flag(int ac, char **av)
 {
 	t_bit	*bit;
@@ -58,23 +58,23 @@ int main(int ac, char **av)
 	t_bit	*flag;
 	t_tree	*tree;
 	t_tree	*tree_er;
-	int test;
 
 	flag = create_flag(ac, av);
 	tree_er = tree_invalid(ac, av);
 	tree = tree_born(flag, ac, av);
-	if (tree)
+/*	if (tree)
 	{
 		tree = tree_to_start(tree);
 		flag->a ? get_totalr(tree) : get_total(tree);
 		tree_sort(tree, size_sort);
-	}
+	} */
 	if (tree_er)
 	{
 		tree_er = tree_to_start(tree_er);
 		tree_sort(tree_er, ascii_sort);
 		tree_er = tree_to_start(tree_er);
 	}
+	tree_controller(tree, flag);
 	tree_print(tree_er, NULL, flag);
 	tree_print(tree, NULL, flag);
 	tree_destroy(tree);
